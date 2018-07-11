59d0be3b9f6dddbfd449c080d81052f9eccbaa7e
wiltrahan <twilorip@gmail.com>
2018-07-11 15:19:09 -0400

    changes

--- src/pair_programming_challenge/Transaction.java
+++ src/pair_programming_challenge/Transaction.java
@@ -1,30 +1,6 @@
 package pair_programming_challenge;
 
-public class Transaction extends BankAccount {
+public class Transaction {
 	
-	double transactionAmount;
-
-	public Transaction(String name, int acctNumber, String branchLocation, double totalBalance, double transactionAmount) {
-		super(name, acctNumber, branchLocation, totalBalance);
-		this.transactionAmount = transactionAmount;
-	}
-
-	public double getAmount() {
-		return transactionAmount;
-	}
-
-	public void setAmount(double transactionAmount) {
-		this.transactionAmount = transactionAmount;
-	}
-	
-	public void theTransaction(double amount, String transactionType) {
-		if(transactionType == "withdraw") {
-			double withdraw = getTotalBalance() - amount;
-			applyTransaction(withdraw);
-		} else if(transactionType == "deposit") {
-			double deposit = getTotalBalance() + amount;
-			applyTransaction(deposit);
-		}
-	}
 
 }
